b569b2aec67be8b9ad25b3eb05ba41c20858b3ec
Pawel L <virtuozoo@o2.pl>
2020-11-04 21:46:33 +0100

    create SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import SvgSearch from './SvgSearch';
+import '../styles/searchbar.scss'
 
 
 const SearchBar = () => {
+
+
     return (
-        <h2>I am searchbar</h2>
+        <div className="search-bar">
+            <div className="search-bar__inner">
+                <button className="search-bar__btn">
+                    <SvgSearch width="20" height="20" fill="#3F3F3F" />
+                </button>
+                <input className="search-bar__input" type="text" placeholder="Search..." />
+            </div>
+        </div>
 
     )
 }
 
 export default SearchBar;
+
